cd3a588d9b063c0709d8a835d7e571820eb825db
Assel Al Assel <aseel1234500000@gmail.com>
2022-04-14 23:29:03 +0300

    the test and code after test

--- src/index.js
+++ src/index.js
@@ -2,47 +2,90 @@
 
 import data from "./data";
 
-export function main(data) {
-  var data1 = [];
-  // iterate through the data
-  for (var i = 0; i < data.length; i++) {
-    var match = false; // initialize the flag
-    for (var j = 0; j < data1.length; j++) {
-      if (data1[j].toLowerCase() == data[i].toLowerCase()) {
+// export function main(data) {
+//   var data1 = [];
+//   // iterate through the data
+//   for (var i = 0; i < data.length; i++) {
+//     var match = false; // initialize the flag
+//     for (var j = 0; j < data1.length; j++) {
+//       if (data1[j].toLowerCase() == data[i].toLowerCase()) {
+//         match = true;
+//         break;
+//       }
+//     }
+//     if (match == false) {
+//       data1[data1.length] = data[i];
+//       var code = data[i].charCodeAt(0);
+//       if (code >= 97 && code <= 122) {
+//         code = code - 32;
+//       }
+//       data1[data1.length - 1] =
+//         String.fromCharCode(code) + data[i].substr(1, data[i].length - 1);
+//       var length = data1.length;
+//       if (length > 1) {
+//         for (var k = length - 1; k >= 1; k--) {
+//           const x = data1[k];
+//           if (data1[k - 1] > x) {
+//             let c = data1[k - 1]; // remember temporary value
+//             data1[k - 1] = x;
+//             data1[k] = c;
+//           }
+//         }
+//       }
+//       var result = Array.prototype.filter.apply(data1, [
+//         (value) => value.search(/ /i) != -1
+//       ]); // important
+//       var num = 1;
+//       for (var line of result) {
+//         result[num - 1] = (num <= 9 ? "0" + num : num) + ' "' + line + '"';
+//         num++; // increase counter
+//       }
+//     }
+//   }
+
+//   return result;
+// }
+
+
+const convertFirstLetterToCapital = text => text[0].toUpperCase() + text.slice(1);
+
+const changeTheStyleForOutput = result => {
+  const outputResult = result.map((element, index) => (index >= 9 ? index : "0" + (index + 1) + ' "' + element + '"'));
+  return outputResult;
+}
+
+const sortResult = result => {
+  for (let index = result.length - 1; index >= 1; index--) {
+    const curr = result[index];
+    if (result[index - 1] > curr) {
+      let temp = result[index - 1];
+      result[index - 1] = curr;
+      result[index] = temp;
+    } else {
+      break;
+    }
+  }
+}
+export const main = data => {
+  let result;
+  if (data.length == 0) {
+    return result;
+  }
+  result = [];
+  data.forEach(statementOfData => {
+    let match = false;
+    for (let statementOfResult of result) {
+      if (statementOfData.toLowerCase() === statementOfResult.toLowerCase()) {
         match = true;
         break;
       }
     }
-    if (match == false) {
-      data1[data1.length] = data[i];
-      var code = data[i].charCodeAt(0);
-      if (code >= 97 && code <= 122) {
-        code = code - 32;
-      }
-      data1[data1.length - 1] =
-        String.fromCharCode(code) + data[i].substr(1, data[i].length - 1);
-      var length = data1.length;
-      if (length > 1) {
-        for (var k = length - 1; k >= 1; k--) {
-          const x = data1[k];
-          if (data1[k - 1] > x) {
-            let c = data1[k - 1]; // remember temporary value
-            data1[k - 1] = x;
-            data1[k] = c;
-          }
-        }
-      }
-      var result = Array.prototype.filter.apply(data1, [
-        (value) => value.search(/ /i) != -1
-      ]); // important
-      var num = 1;
-      for (var line of result) {
-        result[num - 1] = (num <= 9 ? "0" + num : num) + ' "' + line + '"';
-        num++; // increase counter
-      }
+    if (!match && statementOfData.search(/ /i) != -1) {
+      result.push(convertFirstLetterToCapital(statementOfData));
+      sortResult(result);
     }
-  }
-
+  });
+  result = changeTheStyleForOutput(result);
   return result;
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,43 @@
-import {main} from  "./index.js";
-describe("main", () => {
-  it("works", () => {
-    expect(1).toBe(1);
+import { main } from "./index.js";
+import * as matchers from 'jest-extended';
+expect.extend(matchers);
+
+
+describe("testing on main function", () => {
+  it("call main without argument", () => {
+    expect(() => main()).toThrow();
+  });
+
+  it("call main with empty array", () => {
+    expect(main([])).toBeUndefined();
+  });
+
+  it("call main with array have 1 statement and  contain 1 word start with small letter", () => {
+    expect(main(["hello"])).toIncludeSameMembers([]);
+  });
+
+  it("call main with array 1 statement but contain 1 word state with capital", () => {
+    expect(main(["Hello"])).toIncludeSameMembers([]);
+  });
+
+  it("call main with array 1 element start with small letter", () => {
+    expect(main(["hello from me"])).toIncludeSameMembers(['01 "Hello from me"']);
+  });
+
+  it("call main with 1 array statement  start with capital letter", () => {
+    expect(main(["Hello from me"])).toIncludeSameMembers(['01 "Hello from me"']);
+  });
+
+
+  it("call main with many statements but all statements are same (uncase sensitive)", () => {
+    expect(main(["Hello from me", "hello from me", "hello From Me",])).toIncludeSameMembers(['01 "Hello from me"']);
+  });
+
+  it("call main with many statements (sort the array ass)", () => {
+    expect(main(["Hello from me", "hello from you", "hello From him"])).toIncludeSameMembers(['01 "Hello From him"', '02 "Hello from me"', '03 "Hello from you"']);
+  });
+
+  it("call main with array has many statements has one word or multiple", () => {
+    expect(main(["Hello from me", "Hello from me", "hello", "hello from you", "hello From him"])).toIncludeSameMembers(['01 "Hello From him"', '02 "Hello from me"', '03 "Hello from you"']);
   });
 });
